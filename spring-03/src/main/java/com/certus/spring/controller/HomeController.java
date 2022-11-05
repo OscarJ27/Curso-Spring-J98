8307d9e5e6e79fb2411b327bdf23c3d1c3ed20a1
OscarJ27 <81577918+OscarJ27@users.noreply.github.com>
2022-11-05 00:18:35 -0500

    nuevo commit

--- spring-03/src/main/java/com/certus/spring/controller/HomeController.java
+++ spring-03/src/main/java/com/certus/spring/controller/HomeController.java
@@ -3,13 +3,20 @@ package com.certus.spring.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 import com.certus.spring.models.Personaje;
+import com.certus.spring.service.IPersonajeService;
+
 
 @Controller
 @RequestMapping("/app")
@@ -18,26 +25,66 @@ public class HomeController {
 	@Value("${title.generic}")
 	private String titlePage;
 	
+	@Autowired
+	@Qualifier("servicio1")
+	private IPersonajeService InterfacePersonaje1;
+	
+	@Autowired
+	@Qualifier("servicio2")
+	private IPersonajeService InterfacePersonaje2;
+	
+	//private PersonajeService personaje = new PersonajeService();
+	
 	@GetMapping({"/home","/inicio","/","/Home","/Inicio"})
-	public String HolaMundo(Model model) {
+	public String Home(Model model) {
 		
-		Personaje personaje1 = new Personaje();
-		personaje1.setNombres("Luffy");
-		personaje1.setAlias("Luffy Alias");
-		personaje1.setTipoFruta("Luffy TipoFruta");
-		personaje1.setHabilidad("Luffy Habilidad");
-		personaje1.setTripulacion("Luffy Tripulacion");
-		personaje1.setRecompensa("123456789");
-		
-		List<Personaje> listita = new ArrayList<>();
-		listita.add(personaje1);
+	
 		
 		
 		model.addAttribute("TituloPagina", titlePage);
 		model.addAttribute("titulo","Seccion J98");
-		model.addAttribute("listita",listita);
+		
+		List<Personaje> listasUnidas = new ArrayList<>();
+		
+		/*if(InterfacePersonaje1.crearPersonaje().getEstado()) {
+			model.addAttribute("listita",listasUnidas);
+			
+		}*/
+		
+		listasUnidas.addAll(InterfacePersonaje1.crearPersonaje().getData());
+		listasUnidas.addAll(InterfacePersonaje2.crearPersonaje().getData());
+		
+		model.addAttribute("listita", listasUnidas);
+			
+		model.addAttribute("Estado",InterfacePersonaje1.crearPersonaje().getMensaje());
+		
+		Personaje personaje = new Personaje();
+		
+		String respuesta = InterfacePersonaje2.demoMetodo(personaje);
+		
+		model.addAttribute("respuesta",respuesta);
 		
 		return "Home";
 	}
+	
+	@GetMapping("/crear")
+	public String Formulario(Model model) {
+		Personaje personaje =new Personaje();
+		
+		model.addAttribute("TituloPagina", titlePage);
+		model.addAttribute("titulo","Seccion J98 - Crear Personaje");
+		model.addAttribute("personaje",personaje);
+		
+		return "Formulario";
+	}
+	
+	@PostMapping("/form")
+	public String crearPersonaje( Personaje Luffy,Model model) {
+		
+		model.addAttribute("TituloPagina", titlePage);
+		model.addAttribute("titulo","Seccion J98 - Personaje Creado");
+		model.addAttribute("personaje",Luffy);
+		
+		return "Formulario";	}
 
 }
